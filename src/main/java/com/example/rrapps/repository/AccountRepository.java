488fe3d67d346dbfe38c53b847556ec5ab39947c
azavedeeva <anastasia.zavedeeva@endava.com>
2023-09-18 10:46:38 +0300

    Revert "initial commit"
    This reverts commit dca31865f58cd7510e09df8263ec2f053fe61dc9.

--- src/main/java/com/example/rrapps/repository/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.rrapps.repository;
-
-import com.example.rrapps.entity.Account;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface AccountRepository extends ReactiveCrudRepository<Account, Long> {
-
-}
